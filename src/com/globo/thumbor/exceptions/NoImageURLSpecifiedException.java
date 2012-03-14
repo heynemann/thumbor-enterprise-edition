f2be0864c7cbefd1676565f5001ab6ff5647e681
Bernardo Heynemann <heynemann@gmail.com>
2012-03-14 14:19:50 -0300

    No Image URL Specified Exception

---
+++ src/com/globo/thumbor/exceptions/NoImageURLSpecifiedException.java
@@ -0,0 +1,11 @@
+package com.globo.thumbor.exceptions;
+
+public class NoImageURLSpecifiedException extends Exception {
+
+	private static final long serialVersionUID = -2648711194022753360L;
+
+	public NoImageURLSpecifiedException(String message) {
+		super(message);
+	}
+
+}
