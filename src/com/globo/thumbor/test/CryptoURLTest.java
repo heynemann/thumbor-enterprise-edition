69f03728a2c8739d1f330a4c7e6fc0b484092877
Bernardo Heynemann <heynemann@gmail.com>
2012-03-14 16:35:33 -0300

    README

--- src/com/globo/thumbor/test/CryptoURLTest.java
+++ src/com/globo/thumbor/test/CryptoURLTest.java
@@ -31,7 +31,7 @@ public class CryptoURLTest {
 	 * Scenario 3
 	 */
 	@Test
-	public void canEncriptWithMeta() {
+	public void canEncryptWithMeta() {
 		String expectedUrl = "/Jj2Xp-__GWUzZ5zemvPGW2B3j5atA7X1ntF0irz-YGXUcE3-QpqkDbDnVUmBhHi-/my.server.com/some/path/to/image.jpg";
 		assertEquals(expectedUrl, this.cryptoURL.withMetaDataOnly().toString());
 	}
@@ -40,7 +40,7 @@ public class CryptoURLTest {
 	 * Scenario 4
 	 */
 	@Test
-	public void canEncriptWithSmart() {
+	public void canEncryptWithSmart() {
 		String expectedUrl = "/YV6ASUwnbI8XwBw6LpMdv1wy7xC-EHp44LIQqyPYPIqa-dX7JCv4LSeObHxPyY17/my.server.com/some/path/to/image.jpg";
 		assertEquals(expectedUrl, this.cryptoURL.withSmartCropping().toString());
 	}
@@ -49,7 +49,7 @@ public class CryptoURLTest {
 	 * Scenario 5
 	 */
 	@Test
-	public void canEncriptWithFitIn() {
+	public void canEncryptWithFitIn() {
 		String expectedUrl = "/nZlz3CEKZFMVFcNo7KKFzFWKWb7W2fFEqo_LQ2omj13fQPzSSENNk7Iz8Pc4sFen/my.server.com/some/path/to/image.jpg";
 		assertEquals(expectedUrl, this.cryptoURL.fitIn(0,0).toString());
 	}
@@ -58,7 +58,7 @@ public class CryptoURLTest {
 	 * Scenario 6
 	 */
 	@Test
-	public void canEncriptWitnFlipHorizontally() {
+	public void canEncryptWitnFlipHorizontally() {
 		String expectedUrl = "/lMySk3L-Z2oa-RXQs4MgWWB3j5atA7X1ntF0irz-YGXUcE3-QpqkDbDnVUmBhHi-/my.server.com/some/path/to/image.jpg";
 		assertEquals(expectedUrl, this.cryptoURL.flipHorizontally().toString());
 	}
@@ -67,7 +67,7 @@ public class CryptoURLTest {
 	 * Scenario 7
 	 */
 	@Test
-	public void canEncriptWitnFlipVertically() {
+	public void canEncryptWitnFlipVertically() {
 		String expectedUrl = "/Yq1tjo95ZWIKrntANgW-UGB3j5atA7X1ntF0irz-YGXUcE3-QpqkDbDnVUmBhHi-/my.server.com/some/path/to/image.jpg";
 		assertEquals(expectedUrl, this.cryptoURL.flipVertically().toString());
 	}
@@ -76,7 +76,7 @@ public class CryptoURLTest {
 	 * Scenario 8
 	 */
 	@Test
-	public void canEncriptWitnHorizontalAlign() {
+	public void canEncryptWitnHorizontalAlign() {
 		String expectedUrl = "/R7JICjkMQjLpWu7yS49k81wy7xC-EHp44LIQqyPYPIqa-dX7JCv4LSeObHxPyY17/my.server.com/some/path/to/image.jpg";
 		assertEquals(expectedUrl, this.cryptoURL.align(CryptoURL.HAlign.RIGHT).toString());
 	}
@@ -85,7 +85,7 @@ public class CryptoURLTest {
 	 * Scenario 9
 	 */
 	@Test
-	public void canEncriptWitnVerticalAlign() {
+	public void canEncryptWitnVerticalAlign() {
 		String expectedUrl = "/ib7aw8i4eZgB4O1uKGYPK60opAJfqN_5yh0XwqPlyM2mH5xrM-n_C6iUxpp8Tepa/my.server.com/some/path/to/image.jpg";
 		assertEquals(expectedUrl, this.cryptoURL.align(CryptoURL.VAlign.TOP).toString());
 	}
@@ -94,7 +94,7 @@ public class CryptoURLTest {
 	 * Scenario 10
 	 */
 	@Test
-	public void canEncriptWitnCrop() {
+	public void canEncryptWithCrop() {
 		String expectedUrl = "/pppJViujgf3FyaOp5F6fQGIk5HS6ZXXDd-hzIwNvNXOv5wrU_0atPzQvFE2xPgQU/my.server.com/some/path/to/image.jpg";
 		assertEquals(expectedUrl, this.cryptoURL.crop(10, 20, 30, 40).toString());
 	}
